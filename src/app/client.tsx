1a7bc3cb056dc38ac330fa239e06afa782e4b0dc
rutkat <{rutkat}@users.noreply.github.com>
2026-07-01 17:17:55 -0400

    Added trpc api setup

---
+++ src/app/client.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { trpc } from "@/trpc/server";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+export const Client = () => {
+  const trpc = useTRPC();
+  const {data: users} = useSuspenseQuery(trpc.getUsers.queryOptions())
+
+  return (
+    <>Client component: {JSON.stringify(users)}</>
+  )
+}

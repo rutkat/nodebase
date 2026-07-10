612338e3c99789ff1ada0924c0f1ba6ac4d799a7
rutkat <{rutkat}@users.noreply.github.com>
2026-07-09 23:10:56 -0400

    Added sentry error tracking.

---
+++ sentry.server.config.ts
@@ -0,0 +1,27 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://68cd395f9ba6a1b1408f267cef5ec275@o4511705784647681.ingest.us.sentry.io/4511705789956096",
+
+  integrations: [
+    Sentry.vercelAIIntegration({
+      recordInputs: true,
+      recordOutputs: true,
+    }),
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  sendDefaultPii: true,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,24 @@
 "use client";
-import * as Sentry from "@sentry/nextjs";
 import { Button } from "@/components/ui/button";
 import { LogoutButton } from "./logout";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import { toast } from "sonner";
 
-class SentryExampleFrontendError extends Error {
-  constructor(message: string | undefined) {
-    super(message);
-    this.name = "SentryExampleFrontendError";
-  }
-}
-
 const Page = () => {
   
   const trpc = useTRPC();
   const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
   const testAi = useMutation(trpc.testAi.mutationOptions({
-    onSuccess: () => toast.success("AI Job queued"),
-    onError: () => {
-      toast.error("Something went wrong.");
-      throw new SentryExampleFrontendError("This error is raised on the frontend of the example page.");
-    }
-  }))
+    onSuccess: () => toast.success("AI job queued"),
+    onError: () => toast.error("Something went wrong.")
+  }));
 
   const create = useMutation(trpc.createWorkflow.mutationOptions({
     onSuccess: () => toast.success("Job queued")
   }));
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       protected server component

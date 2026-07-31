e5508229301c0d62daa2763f8791200a27b71581
rutkat <{rutkat}@users.noreply.github.com>
2026-07-30 20:34:25 -0400

    Added http manual trigger inngest-realtime to node executions.

---
+++ src/features/triggers/components/manual-trigger/actions.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import { getSubscriptionToken, type Realtime } from "@inngest/realtime";
+import { inngest } from "@/inngest/client";
+import { manualTriggerChannel } from "@/inngest/channels/manual-trigger";
+
+export type manualTriggerToken = Realtime.Token<typeof manualTriggerChannel, ["status"]>;
+
+export async function fetchManualTriggerRealtimeToken(): Promise<ManualTriggerToken> {
+  const token = await getSubscriptionToken(inngest, {
+    channel: manualTriggerToken(),
+    topics: ["status"],
+  });
+
+  return token;
+}

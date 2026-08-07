9c5b6dc9df05403d669fb7ade75d3dc883b1d720
rutkat <{rutkat}@users.noreply.github.com>
2026-08-07 13:59:04 -0400

    Added Slack and Discord nodes to workflow with webhooks to automate messaging to Discord and Slack channels.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -10,6 +10,8 @@ import { googleFormTriggerChannel } from "./channels/google-form-trigger";
 import { geminiChannel } from "./channels/gemini";
 import { openAiChannel } from "./channels/openai";
 import { anthropicChannel } from "./channels/anthropic";
+import { discordChannel } from "./channels/discord";
+import { slackChannel } from "./channels/slack";
 
 export const executeWorkflow = inngest.createFunction(
   { id: "execute-workflow" },
@@ -22,6 +24,8 @@ export const executeWorkflow = inngest.createFunction(
       geminiChannel(),
       openAiChannel(),
       anthropicChannel(),
+      discordChannel(),
+      slackChannel(),
     ],
   },
   async ({ event, step }) => {
@@ -43,6 +47,17 @@ export const executeWorkflow = inngest.createFunction(
       return topologicalSort(workflow.nodes, workflow.connections);
     });
 
+    const userId = await step.run("find-user-id", async () => {
+      const workflow = await prisma.workflow.findUniqueOrThrow({
+        where: { id: workflowId },
+        select: {
+          userId: true,
+        },
+      });
+
+      return workflow.userId;
+    });
+
     let context = event.data.initialData || {};
     
     for (const node of sortedNodes) {
@@ -50,6 +65,7 @@ export const executeWorkflow = inngest.createFunction(
       context = await executor({
         data: node.data as Record<string, unknown>,
         nodeId: node.id,
+        userId,
         context,
         step,
       });
